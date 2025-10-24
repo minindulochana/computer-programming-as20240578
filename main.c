4ac4fced7c222633b43b211a2f83da9d812c6336
minindulochana <lochanadharmarathne@gmail.com>
2025-10-24 15:53:41 +0530

    added menu

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+int main() {
+
+
+     while (choice !=10) {
+            int choice;
+        printf("\n---- Logistics Management System ----\n");
+        printf("1. City Management\n");
+        printf("2. Distance Management\n");
+        printf("3. Vehicle Management\n");
+        printf("4. Delivery Request Handling\n");
+        printf("5. Cost, Time, and Fuel Calculations\n");
+        printf("6. Delivery Records\n");
+        printf("7. Finding The Least-Cost Route (Least-Distance)\n");
+        printf("8. Performance Reports\n");
+        printf("9. File Handling\n");
+        printf("10. Exit\n");
+        printf("Choose option: ");
+     scanf("%d",&choice);
+
+
+
+        switch (choice) {
+            case 1:
+                printf("not added yet");
+                break;
+                case 2:
+                printf("not added yet");
+                break;
+                case 3:
+                printf("not added yet");
+                break;
+                case 4:
+                printf("not added yet");
+                break;
+                case 5:
+                printf("not added yet");
+                break;
+                case 6:
+                printf("not added yet");
+                break;
+                case 7:
+                printf("not added yet");
+                break;
+                case 8:
+                printf("not added yet");
+                break;
+                case 9:
+                printf("not added yet");
+                break;
+                case 10:
+                printf("not added yet");
+                break;
+                default:
+            printf("Invalid choice.\n");
+
+
+
+
+        }
+    }
+}
