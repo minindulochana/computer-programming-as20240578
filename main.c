637e42bd79b6dda17951d337ae2177bf1ff03e95
minindulochana <lochanadharmarathne@gmail.com>
2025-10-24 16:42:59 +0530

    modified menu and added addcity and removenewline functions

--- main.c
+++ main.c
@@ -2,13 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define CITIES 30
+#define SIZE 50
+void removeNewline(char *str);
+void addCity(char city[][SIZE], int *citycount);
+void cityManagement(char city[][SIZE], int *citycount);
+
 
 
 int main() {
+    char cities[CITIES][SIZE];
+int citycount = 0;
 
-
+ int choice;
      while (choice !=10) {
-            int choice;
+
         printf("\n---- Logistics Management System ----\n");
         printf("1. City Management\n");
         printf("2. Distance Management\n");
@@ -27,7 +35,7 @@ int main() {
 
         switch (choice) {
             case 1:
-                printf("not added yet");
+                cityManagement(cities, &citycount);
                 break;
                 case 2:
                 printf("not added yet");
@@ -54,7 +62,7 @@ int main() {
                 printf("not added yet");
                 break;
                 case 10:
-                printf("not added yet");
+                printf("you exit the program\n");
                 break;
                 default:
             printf("Invalid choice.\n");
@@ -65,3 +73,64 @@ int main() {
         }
     }
 }
+
+
+
+void removeNewline(char *str) {
+    int len = strlen(str);
+    if (str[len - 1] == '\n')
+        str[len - 1] = '\0';
+}
+
+// to add a new city
+void addCity(char city[][SIZE], int *citycount) {
+    if (*citycount >= CITIES) {
+        printf("City limit reached!\n");
+        return;
+    }
+
+    printf("Enter city name: ");
+    fgets(city[*citycount], SIZE, stdin);
+    removeNewline(city[*citycount]);
+
+    (*citycount)++;
+    printf("City added successfully!\n");
+}
+
+
+
+
+void cityManagement(char city[][SIZE], int *citycount) {
+    int choice_city;
+    while (1) {
+        printf("\n--- City Management ---\n");
+        printf("1) Add City\n");
+        printf("2) Rename City\n");
+        printf("3) Remove City\n");
+        printf("4) Display Cities\n");
+        printf("0) Back to Main Menu\n");
+        printf("Enter option: ");
+        scanf("%d", &choice_city);
+        getchar();
+
+        switch (choice_city) {
+            case 1:
+                addCity(city, citycount);
+                break;
+            case 2:
+                printf("not added yet");
+                break;
+            case 3:
+                printf("not added yet");
+                break;
+            case 4:
+                printf("not added yet");
+                break;
+            case 0:
+                return;
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+}
+
